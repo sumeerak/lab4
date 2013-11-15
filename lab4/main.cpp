46adacc52531ceb60c0b8c80bae21d0c54f61eea
sumeerak <sumkhalidy@gmail.com>
2013-11-15 15:35:21 -0500

    example

---
+++ lab4/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: skhalidy
+ */
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x =10;
+	int y = 15;
+	int total;
+	total = x + y;
+	cout << "Total:"<< total;
+}
